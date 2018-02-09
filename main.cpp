8e04568cfa895759e4369e2c0a2531ad2163a2aa
aalenezi1 <aalenezi1@mail.csuchico.edu>
2018-02-08 22:31:30 -0800

    end of the work

--- main.cpp
+++ main.cpp
@@ -6,19 +6,30 @@ using namespace std;
 int main()
 {
 
+char edge;
+
   cout<<"What character do you want to know about?\n";
+  cin>>edge;
+
+    if(edge>='A' && edge<='z')
+    {
 
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout<<edge<<" is an upper case letter!\n";
+}else if (edge>='a' && edge<='z')
+{
 
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
-
+  cout<<edge<<" is a lower case letter!\n";
+}else
+{
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
+  cout<<edge<<"?! Pssh. What are you talking about?\n";
+ }
+
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int)edge<<endl;
+
 
   return 0;
 }
